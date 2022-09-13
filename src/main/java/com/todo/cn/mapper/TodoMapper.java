196a3de08e45281fefdad93e534e54a3c3bd6c7c
nohyunha95 <nohyunha95@gmail.com>
2022-09-13 23:16:20 +0900

    해결

--- src/main/java/com/todo/cn/mapper/TodoMapper.java
+++ src/main/java/com/todo/cn/mapper/TodoMapper.java
@@ -2,10 +2,7 @@ package com.todo.cn.mapper;
 
 import com.todo.cn.controller.dto.PostTodoReq;
 import org.apache.ibatis.annotations.Mapper;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-=======
->>>>>>> d1c17413a6a1b3f7ddf92c9d9919f52262e54b8d
+
 
 @Mapper
 public interface TodoMapper {
