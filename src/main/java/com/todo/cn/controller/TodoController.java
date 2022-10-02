369acac2d21207a543d4dd64a286d1b1ef29b43b
chaesuhyeon <ddd8177@gmail.com>
2022-10-02 22:34:10 +0900

    thymeleaf로 하기 위해 컨트롤러 수정

--- src/main/java/com/todo/cn/controller/TodoController.java
+++ src/main/java/com/todo/cn/controller/TodoController.java
@@ -4,24 +4,63 @@ import com.todo.cn.controller.dto.GetTodoRes;
 import com.todo.cn.controller.dto.PostTodoReq;
 import com.todo.cn.domain.TodoVO;
 import com.todo.cn.service.TodoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-@RestController
+//@RestController
+@Controller
 @RequestMapping("/todo")
+@RequiredArgsConstructor
 public class TodoController {
 
-    @Autowired
-    private TodoService service;
+    private final TodoService service;
 
+    @GetMapping("/list/{uno}")
+    public String list(@PathVariable int uno , Model model){
+        List<GetTodoRes> list = service.getList(uno);
+        model.addAttribute("list" , list);
+
+        return "list";
+    }
+
+    @PostMapping("/register")
+    public String register(PostTodoReq ptr , RedirectAttributes redirectAttributes){
+        service.register(ptr);
+        redirectAttributes.addAttribute("uno", ptr.getUno());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/todo/list/{uno}";
+    }
+
+    @PostMapping("/modify/{tno}")
+    public String update(@PathVariable int tno, PostTodoReq ptr , RedirectAttributes redirectAttributes){
+        service.modify(ptr);
+        redirectAttributes.addAttribute("uno", ptr.getUno());
+        return "redirect:/todo/list/{uno}";
+
+    }
+
+    @PostMapping("/delete/{tno}")
+    public String delete(@PathVariable int tno , RedirectAttributes redirectAttributes){
+        TodoVO todoVO = service.selectTodo(tno);
+        service.removeAndInsertBin(todoVO);
+        redirectAttributes.addAttribute("uno", todoVO.getUno());
+        return "redirect:/todo/list/{uno}";
+
+    }
+
+//    @PostMapping("/restore/{tno}")
+//    public String restoreTodo(@PathVariable int tno){
+//        service.restoreTodo(tno);
+//        return
+//    }
+
+/**
+ * API
     @PostMapping(value = "/register" , consumes = "application/json" , produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Integer> create(@RequestBody PostTodoReq ptr){
         int result = service.register(ptr);
@@ -60,5 +99,5 @@ public class TodoController {
         service.restoreTodo(tno);
 
     }
-
+*/
 }
