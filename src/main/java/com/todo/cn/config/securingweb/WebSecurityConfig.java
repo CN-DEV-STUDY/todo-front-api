3b201ac723b64047c3127d3956d7785340302527
nohyunha95 <nohyunha95@gmail.com>
2022-10-16 16:33:54 +0900

    메인 페이지 수정

--- src/main/java/com/todo/cn/config/securingweb/WebSecurityConfig.java
+++ src/main/java/com/todo/cn/config/securingweb/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,5 +41,6 @@ public class WebSecurityConfig {
 
         return new InMemoryUserDetailsManager(user);
     }
+
 }
 
